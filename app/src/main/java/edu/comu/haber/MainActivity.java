b6391cd1e874fd91bbd81b4f7cc08d46aafbf642
samed <naturalfusen2@gmail.com>
2019-04-23 17:35:54 +0300

    Final version

--- app/src/main/java/edu/comu/haber/MainActivity.java
+++ app/src/main/java/edu/comu/haber/MainActivity.java
@@ -1,13 +1,11 @@
 package edu.comu.haber;
 
-import android.app.Notification;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -18,7 +16,13 @@ import android.widget.Toast;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+import androidx.work.Constraints;
+import androidx.work.ExistingPeriodicWorkPolicy;
+import androidx.work.NetworkType;
+import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkManager;
 import edu.comu.haber.webpages.MainPage;
 import edu.comu.haber.webpages.model.News;
 
@@ -35,21 +39,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listView= (ListView)findViewById(R.id.listview);
-        bar = (ProgressBar)findViewById(R.id.bar);
+        listView = (ListView) findViewById(R.id.listview);
+        bar = (ProgressBar) findViewById(R.id.bar);
 
-        if (appStatus.isOnline(getApplicationContext())) { //when internet connection is aviable
-       /*  MainPage mainPage = new MainPage();
-
-            mainPage.setLastNews(getApplicationContext(),"ÇOMÜ – Ziraat Sigorta Arasında BES Protokolü 1 Yıl Daha Uzatıldı");
-            Log.d("gel",mainPage.getLastNews(getApplicationContext()));
-*/
-     new  GetNews().execute();
-
+        if (appStatus.isOnline(getApplicationContext())) {
+            new GetNews().execute();
         } else {
-
+            Toast.makeText(MainActivity.this,"İnternet Bağlantısı Yok! Uygulamayı Yeniden Başlatın",Toast.LENGTH_LONG).show();
         }
-
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -58,27 +55,12 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(browserIntent);
             }
         });
-
-        Notification.Builder nb;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            mNotificationUtils = new NotificationUtils(this);
-
-
-            nb = mNotificationUtils.getAndroidChannelNotificationForOreo("yarak", "By " + "çük");
-            mNotificationUtils.getManager().notify(101, nb.build());
-        }else{
-
-            nb = mNotificationUtils.getAndroidChannelNotificationFor("yarak", "By " + "çük");
-            mNotificationUtils.getManager().notify(101, nb.build());
-        }
-
-
-
-
-
     }
-    class GetNews extends AsyncTask<Void,Void, List<News>> {
+
+
+    class GetNews extends AsyncTask<Void, Void, List<News>> {
         MainPage mainPage = new MainPage();
+
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
@@ -87,7 +69,6 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected List<News> doInBackground(Void... voids) {
-
             try {
                 mainPage.connectToPage();
             } catch (IOException e) {
@@ -103,20 +84,20 @@ public class MainActivity extends AppCompatActivity {
             List<String> title = new ArrayList<String>();
             String newestNews = news.get(0).getTitle();
 
-           for(int i = 0; i< news.size(); i++) {
-             title.add(news.get(i).getTitle());
-           }
+            for (int i = 0; i < news.size(); i++) {
+                title.add(news.get(i).getTitle());
+            }
 
-           for(int  i = 0; i< title.size(); i++){
-               if (title.indexOf(mainPage.getLastNews(getApplicationContext())) == i || mainPage.getLastNews(getApplicationContext()).equals("false")){
-                   mainPage.setLastNews(getApplicationContext(),newestNews);
-                   break;
-               }else{
-                   title.set(i,"-YENİ HABER- "+title.get(i));
-               }
-           }
+            for (int i = 0; i < title.size(); i++) {
+                if (title.indexOf(mainPage.getLastNews(getApplicationContext())) == i || mainPage.getLastNews(getApplicationContext()).equals("false")) {
+                    mainPage.setLastNews(getApplicationContext(), newestNews);
+                    break;
+                } else {
+                    title.set(i, "-YENİ HABER- " + title.get(i));
+                }
+            }
 
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,android.R.id.text1,title);
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, android.R.id.text1, title);
             listView.setAdapter(adapter);
 
             bar.setVisibility(View.GONE);
@@ -124,7 +105,11 @@ public class MainActivity extends AppCompatActivity {
 
             newsList = news;
 
-        }
+            Constraints constraints = new Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build();
 
+            PeriodicWorkRequest builder = new PeriodicWorkRequest.Builder(CheckNews.class,15, TimeUnit.MINUTES).setConstraints(constraints).build();
+            WorkManager.getInstance().enqueueUniquePeriodicWork("check",  ExistingPeriodicWorkPolicy.KEEP,builder);
+
+        }
     }
 }
